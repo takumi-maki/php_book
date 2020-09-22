38e51f8a7defbaff12c0739bd146e487d100d152
takumiiimo <assistant2370@gmail.com>
2020-09-22 23:44:49 +0900

    ログアウトを設置する #10

---
+++ post/logout.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+// セッションを削除
+$_SESSION = array();
+if(ini_get("session.use_cookies")){
+    $params = session_get_cookie_params();
+    setcookie(session_name(), ''. time() - 4200,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+);
+}
+session_destroy();
+
+// cookie情報を削除
+setcookie('email', '', time() - 3600);
+setcookie('password', '', time() - 3600);
+
+header('Location: login.php');
+?>
